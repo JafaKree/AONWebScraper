3b8b88a8e6bd2de01139878b735f84b8104b457d
Dylan Powell <dylan.loy.powell1@gmail.com>
2024-09-04 21:10:18 -0700

    Initial commit

---
+++ AONWebScraper/WebScraper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+public class WebScraper
+{
+    private readonly HttpClient _httpClient;
+
+    public WebScraper()
+    {
+        _httpClient = new HttpClient();
+    }
+
+    public async Task<string> GetPageContentAsync(string url)
+    {
+        try
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine($"Request error: {e.Message}");
+            return null;
+        }
+    }
+
+    public HtmlDocument LoadHtmlDocument(string htmlContent)
+    {
+        var document = new HtmlDocument();
+        document.LoadHtml(htmlContent);
+        return document;
+    }
+
+    public void PrintNodesText(HtmlDocument document, string xPath)
+    {
+        var nodes = document.DocumentNode.SelectNodes(xPath);
+        if (nodes != null)
+        {
+            foreach (var node in nodes)
+            {
+                Console.WriteLine(node.InnerText);
+            }
+        }
+        else
+        {
+            Console.WriteLine("No nodes found for the given XPath.");
+        }
+    }
+    public HtmlNodeCollection GetNodes(HtmlDocument document, string xPath)
+    {
+        return document.DocumentNode.SelectNodes(xPath);
+    }
+}
---
+++ AONWebScraperTests/WebScraperTests.cs
@@ -0,0 +1,42 @@
+﻿using HtmlAgilityPack;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class WebScraperTests
+    {
+
+        [TestMethod()]
+        public async Task GetPageContentAsyncTestAsync()
+        {
+            WebScraper scraper = new WebScraper();
+            string url = "https://2e.aonprd.com/";
+            string htmlContent = await scraper.GetPageContentAsync(url);
+
+            if (htmlContent != null)
+            {
+                HtmlDocument document = scraper.LoadHtmlDocument(htmlContent);
+                Assert.IsNotNull(document);
+                string userProfilePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+                document.Save($"{userProfilePath}\\Desktop\\example.html");
+            }
+            else
+            {
+                Console.WriteLine("Failed to get page content.");
+            }
+        }
+    }
+}
+
+namespace AONWebScraperTests
+{
+    internal class WebScraperTests
+    {
+    }
+}
